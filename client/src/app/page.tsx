7855b2315ebcbffba7101afde9fdbe1fc0da9414
Sainesh25 <patilsainesh25@gmail.com>
2025-01-12 23:02:50 +0530

    fix

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HomePage from '@/components/HomePage/HomePage'
 
 export default function Home() {
+  
   return (
     <>
       <HomePage/>
--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
 import Link from 'next/link'
-export default function HomePage() {
+import { getProducts } from '@/app/service/api'
+
+export async function getServerSideProps() {
+    try {
+        const products = await getProducts();
+        return {
+            props: {
+                products,
+            },
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            props: {
+                products: [], // Fallback in case of an error
+            },
+        };
+    }
+}
+export default function HomePage({ products }) {
+
+
+
     return (
         <>
-            <div className="flex justify-center min-h-screen items-center">
+            <div className="flex bg-slate-900 justify-center min-h-screen items-center">
+                {products?.map((product) => (
+                    <div key={product.id} className="text-white">
+                        {product.name}
+                    </div>
+                ))}
                 <Link href={'/register'} className='bg-sky-600 m-6 text-center rounded-md w-32 p-2 text-white'>Register</Link>
                 <Link href={'/login'} className='bg-sky-600 m-6 text-center rounded-md w-32 p-2 text-white'>Login</Link>
             </div>
